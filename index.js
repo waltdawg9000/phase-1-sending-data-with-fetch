c161c795fdb58a1845d4e854ef19545421e923ee
Walter Morris <waltercmorris18@gmail.com>
2022-01-03 22:25:40 -0800

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Add your code here
+const mainUrl = "http://localhost:3000/users"
+ 
+const bodyTag = document.querySelector('body')
+
+let dataBody = {
+    name: "Steve",
+    email: "steve@steve.com"
+}
+
+let configObjects = {
+    method: "Post",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify(dataBody)
+}
+
+function submitData() {
+    return fetch(mainUrl, configObjects)
+    .then(response => response.json())
+    .then(getID)
+    .catch(errorFunc)
+}
+
+function getID(object) {
+    document.body.innerHTML = object.id
+}
+
+function errorFunc(error){
+    document.body.innerHTML = error.message
+}
